f3cc7ebfbad7698fdd9a832f9586da11fb893c30
Daniele <eaglw99@gmail.com>
2025-11-11 00:38:24 +0100

    Tutorial CSTR quasi funzionante ma non promette molto bene

---
+++ IrreversibleCSTR_PINNs.py
@@ -0,0 +1,167 @@
+from PIL import Image
+import numpy as np
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import os
+
+step=10000
+
+
+
+def save_gif_PIL(outfile, files, fps=5, loop=0, delete_files=False):
+    "Helper function for saving GIFs, modificata per rimuovere i file"
+    imgs = [Image.open(file) for file in files]
+    imgs[0].save(fp=outfile, format='GIF', append_images=imgs[1:], save_all=True, duration=int(1000/fps), loop=loop)
+    if delete_files:
+        for file in files:
+            try:
+                os.remove(file)
+            except Exception as e:
+                print(f"Warning: unable to delete file {file}. Error: {e}")
+
+class FCN(nn.Module):
+    "Defines a connected network"
+    
+    def __init__(self, N_INPUT, N_OUTPUT, N_HIDDEN, N_LAYERS):
+        super().__init__()
+        activation = nn.Tanh
+        self.fcs = nn.Sequential(*[
+                        nn.Linear(N_INPUT, N_HIDDEN),
+                        activation()])
+        self.fch = nn.Sequential(*[
+                        nn.Sequential(*[
+                            nn.Linear(N_HIDDEN, N_HIDDEN),
+                            activation()]) for _ in range(N_LAYERS-1)])
+        self.fce = nn.Linear(N_HIDDEN, N_OUTPUT)
+        
+    def forward(self, x):
+        x = self.fcs(x)
+        x = self.fch(x)
+        x = self.fce(x)
+        return x
+
+def CSTR(F, V, cAin, k, cA0,  x):
+    """Defines the analytical solution to CSTR irreversible reaction"""
+    den = (F+V*k)
+    exp = torch.exp(-(x*den/V))
+    A=(F*cA0 -F*cAin +V*cA0*k)
+    B = (F*cAin)
+    y  = (exp*A+B)/den
+    return y
+
+
+F, V, cAin, k, cA0 = 400, 2000, 10, 1, 10
+
+# get the analytical solution over the full domain
+x = torch.linspace(0,5,500).view(-1,1)
+y = CSTR(F, V, cAin, k, cA0,  x).view(-1,1)
+print(x.shape, y.shape)
+
+# slice out a small number of points from the LHS of the domain
+x_data = x[0:150:10]
+y_data = y[0:150:10]
+print(x_data.shape, y_data.shape)
+
+plt.figure()
+plt.plot(x, y, label="Exact solution")
+plt.scatter(x_data, y_data, color="tab:orange", label="Training data")
+plt.legend()
+plt.show()
+
+
+
+
+
+def plot_result(x,y,x_data,y_data,yh,xp=None):
+    "Pretty plot training results"
+    plt.figure(figsize=(8,4))
+    plt.plot(x,y, color="grey", linewidth=2, alpha=0.8, label="Exact solution")
+    plt.plot(x,yh, color="tab:blue", linewidth=4, alpha=0.8, label="Neural network prediction")
+    plt.scatter(x_data, y_data, s=60, color="tab:orange", alpha=0.4, label='Training data')
+    if xp is not None:
+        plt.scatter(xp, -0*torch.ones_like(xp), s=60, color="tab:green", alpha=0.4, 
+                    label='Physics loss training locations')
+    l = plt.legend(loc=(1.01,0.34), frameon=False, fontsize="large")
+    plt.setp(l.get_texts(), color="k")
+    #plt.xlim(-0.05, 1.05)
+    #plt.ylim(-1.1, 1.1)
+    plt.text(1.065,0.7,"Training step: %i"%(i+1),fontsize="xx-large",color="k")
+    plt.axis("off")
+    
+    
+# train standard neural network to fit training data
+torch.manual_seed(123)
+model = FCN(1,1,32,3)
+optimizer = torch.optim.Adam(model.parameters(),lr=1e-3)
+files = []
+for i in range(step):
+    optimizer.zero_grad()
+    yh = model(x_data)
+    loss = torch.mean((yh-y_data)**2)# use mean squared error
+    loss.backward()
+    optimizer.step()
+    
+    
+    # plot the result as training progresses
+    if (i+1) % 10 == 0: 
+        
+        yh = model(x).detach()
+        
+        plot_result(x,y,x_data,y_data,yh)
+        
+        file = "plots/CSTRnn_%.8i.png"%(i+1)
+        plt.savefig(file, bbox_inches='tight', pad_inches=0.1, dpi=100, facecolor="white")
+        files.append(file)
+    
+        if (i+1) % 5000 == 0: plt.show()# cambiato per non vedere sempre
+        else: plt.close("all")
+            
+save_gif_PIL("CSTRnn.gif", files, fps=20, loop=0,delete_files=True)
+
+
+
+
+x_physics = torch.linspace(0,5,50).view(-1,1).requires_grad_(True)# sample locations over the problem domain
+
+
+torch.manual_seed(123)
+model = FCN(1,1,32,3)
+optimizer = torch.optim.Adam(model.parameters(),lr=1e-4)
+files = []
+for i in range(step):
+    optimizer.zero_grad()
+    
+    # compute the "data loss"
+    yh = model(x_data)
+    loss1 = torch.mean((yh-y_data)**2)# use mean squared error
+    
+    # compute the "physics loss"
+    yhp = model(x_physics)
+    dx  = torch.autograd.grad(yhp, x_physics, torch.ones_like(yhp), create_graph=True)[0]# computes dy/dx
+    physics = dx + k*yhp - F*(cAin-yhp)/V# computes the residual of the 1D harmonic oscillator differential equation
+    loss2 = (1e-4)*torch.mean(physics**2)
+    # penso che qua sotto yhp è cA(t), dx sarebbe dcA/dt
+    
+    # backpropagate joint loss
+    loss = loss1 + loss2# add two loss terms together
+    loss.backward()
+    optimizer.step()
+    
+    
+    # plot the result as training progresses
+    if (i+1) % 10 == 0: 
+        
+        yh = model(x).detach()
+        xp = x_physics.detach()
+        
+        plot_result(x,y,x_data,y_data,yh,xp)
+        
+        file = "plots/CSTRpinn_%.8i.png"%(i+1)
+        plt.savefig(file, bbox_inches='tight', pad_inches=0.1, dpi=100, facecolor="white")
+        files.append(file)
+        
+        if (i+1) % 4500 == 0: plt.show()
+        else: plt.close("all")
+            
+save_gif_PIL("CSTRpinn.gif", files, fps=20, loop=0)
