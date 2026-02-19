ef59861699861a1e9653f1d3e4286a2c9de96641
Daniele <eaglw99@gmail.com>
2026-02-19 20:48:44 +0100

    Gradient clipping added to NNrand and NNgrid

--- Heat2D/src/Heat2D_NN.py
+++ Heat2D/src/Heat2D_NN.py
@@ -74,6 +74,8 @@ def train_modelNN(
         loss = model.loss_fn(pred, T_train)
         
         loss.backward()
+        # Gradient Clipping
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer.step()
         
         # Step dello scheduler
--- Heat2D/src/Heat2D_NN_griglia.py
+++ Heat2D/src/Heat2D_NN_griglia.py
@@ -67,6 +67,8 @@ def train_modelNN_griglia(
         loss = model.loss_fn(pred, T_train)
         
         loss.backward()
+        # Gradient Clipping
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer.step()
         
         # Step dello scheduler
