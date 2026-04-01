15d7c388e2b6839d7c4115b30dae114f5485b390
Daniele <eaglw99@gmail.com>
2026-04-01 22:16:17 +0200

    autoresearch iter 16: Increase L-BFGS iterations to 2000

--- heat2dmini/verify_metric.py
+++ heat2dmini/verify_metric.py
@@ -9,9 +9,10 @@ def run_verify():
     cmd = [
         "python", "heat2dmini/Heat2D_adaptive_mini.py",
         "--epochs", "5000",
-        "--arch", "140,120,100,80,60,50,40,30,20",
+        "--arch", "140,120,100,80,60,40,30,20",
         "--bc_weight", "50",
-        "--act", "GELU"
+        "--act", "GELU",
+        "--lbfgs_iter", "2000"
     ]
     
     try:
