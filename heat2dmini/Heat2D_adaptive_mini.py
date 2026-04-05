e1e131aa5e00d2acf257922e7d0f5f46adbcd0c2
Daniele <eaglw99@gmail.com>
2026-04-05 12:13:07 +0200

    Revert "autoresearch iter 32: increase n_collocation from 40 to 50"
    This reverts commit 7a0ba4194bd490c0d521caeaf09b34cc98b64c13.

--- heat2dmini/Heat2D_adaptive_mini.py
+++ heat2dmini/Heat2D_adaptive_mini.py
@@ -22,7 +22,7 @@ parser.add_argument('--epochs', type=int, default=2500, help='Adam epochs')
 parser.add_argument('--lbfgs_iter', type=int, default=1500, help='L-BFGS iterations')
 parser.add_argument('--bc_weight', type=float, default=25.0, help='Initial BC weight')
 parser.add_argument('--seed', type=int, default=123, help='Random seed')
-parser.add_argument('--n_collocation', type=int, default=50, help='Collocation points')
+parser.add_argument('--n_collocation', type=int, default=40, help='Collocation points')
 args = parser.parse_args()
 
 torch.manual_seed(args.seed)
